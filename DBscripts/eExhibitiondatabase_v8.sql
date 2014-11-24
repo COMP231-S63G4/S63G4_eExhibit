1759abcbaf4cf11579711c65337a0764fb5f78f0
Raminder Singh Sandhu <sandhuraminder2112@gmail.com>
2014-11-23 23:42:34 -0500

    Create eExhibitiondatabase_v8.sql

---
+++ DBscripts/eExhibitiondatabase_v8.sql
@@ -0,0 +1,32 @@
+CREATE DATABASE  IF NOT EXISTS `eexhibition` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `eexhibition`;
+
+-- MySQL dump 10.13  Distrib 5.6.17, for Win64 (x86_64)
+--
+-- Host: localhost    Database: eexhibition
+-- ------------------------------------------------------
+-- Server version	5.6.21-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Bidding Product`
+--
+
+CREATE TABLE `Ticket Detail` (
+  `EventName` varchar(30) NOT NULL,
+  `EventTime` varchar(30) NOT NULL,
+  `Eventlocation`` date DEFAULT NULL,
+  `Eventdate` date DEFAULT NULL,
+  `Uname` varchar(30) NOTNULL,
+   PRIMARY KEY (`eventid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
